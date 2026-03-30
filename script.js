4ed9469832b6ad11732c8aa9b5048df130dca67a
Firee <newowner@News-MacBook-Pro.local>
2026-03-30 01:40:48 -0400

    added a delete function with local storarge

--- script.js
+++ script.js
@@ -4,6 +4,62 @@ let posts = [];
 
 let editingId = null;
 
-const form = document.querySelector('#post-form');
-const titleInput = document.querySelector('');
 
+
+
+///Elements from html
+
+const form = document.querySelector('#post-form');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+const titleError = document.querySelector('#title-error');
+const contentInput = document.querySelector('#content-error');
+const submitBtn = document.querySelector('#submit-btn');
+const cancelBtn = document.querySelector('#cancel-btn');
+const postsContainer = document.querySelector('#posts-container');
+
+// Gets the date of the day 
+
+function getTimestamp() {
+    return new Date().toLocaleDateString('en-Us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+
+    });
+}
+
+// Saving to local storage
+function loadFromStorage() {
+    const saved = localStorage.getItem('posts')
+
+    if (saved) { // something being saved before 
+        posts = JSON.parse(saved);
+    }
+}
+
+
+
+// Reseting the form
+function resetForm() {
+    titleInput = ''; //  Title should be clean
+    contentInput = ''; // content should be clean
+    titleError.style.display = 'none'; //error messages 
+    contentError.style.display = 'none';
+
+
+
+    editingId = null;
+
+    submitBtn.textContent = 'Add post'
+    cancelBtn.style.display = 'none'
+
+
+}
+
+//validating form
+
+function validateForm() {
+
+}
